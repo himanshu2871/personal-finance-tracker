1366db2a83f6844d59972108ac9c20a491c2c3ad
himanshu2871 <115879998+himanshu2871@users.noreply.github.com>
2025-04-16 12:05:07 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ import BudgetVsActualChart from '@/components/BudgetVsActualChart';
 import { format } from 'date-fns';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Button } from '@/components/ui/button';
 
 
 interface Transaction {
